fde92d1b79d586aedcd31bf684f240c2ae7f2e1a
karin <matsukari66@gmail.com>
2026-04-29 06:20:25 -0300

    f commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int , char **argv)
+{
+	int time = 60 * atoi(argv[1]);
+
+	for (int i = 0; i < time; i++)
+	{
+		printf("%d\n", i);	
+		sleep(1);
+	}
+	printf("%d\n", time);	
+			
+	system("notify-send 'boa!' 'sessão finalizada'");
+	//log_to_file((float)time/60);
+	return 0;
+}
