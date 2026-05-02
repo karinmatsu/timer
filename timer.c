0fe497e7bc4e4994732e5f1beb8ba06227fa33ab
karin <matsukari66@gmail.com>
2026-05-02 05:49:13 -0300

    pause added

--- timer.c
+++ timer.c
@@ -14,7 +14,7 @@ static void draw_finish_message();
 static WINDOW *build_timer();
 static void draw_time(int time);
 
-int g_timer_interrupt, g_time_upper_limit, g_timer_finished;
+int g_timer_interrupt, g_time_upper_limit, g_timer_finished, g_time_backup;
 clock_t g_sys_time_base;
 
 static WINDOW *build_timer()
@@ -43,6 +43,12 @@ static void draw_time(int time)
 	wrefresh(g_timer_win);
 }
 
+void draw_paused_state()
+{
+	mvwprintw(g_timer_win, 0, 1, "paused");
+	wrefresh(g_timer_win);	
+}
+
 /* 
 ============================
 INTERFACE
@@ -53,6 +59,7 @@ void timer_initialize()
 {
 	g_timer_finished = 0;
 	g_timer_interrupt = 0;
+	g_time_backup = 0;
 	time(&g_sys_time_base);
 	
 	g_timer_win = build_timer();
@@ -71,18 +78,25 @@ void timer_set_time(int time, short time_scale)
 int timer_update_timer()
 {
 	if (g_timer_finished) return TIME_ENDED;
+	if (g_timer_interrupt) return INTERR;
 	
-	time_t current_time;
-	time(&current_time);
-	double diff = difftime(current_time, g_sys_time_base);
+	time_t current_sys_time;
+	time(&current_sys_time);
+	g_time_backup = difftime(current_sys_time, g_sys_time_base);
 	
-	if (diff >= g_time_upper_limit)
+	if (g_time_backup >= g_time_upper_limit)
 	{
 		g_timer_finished = 1;
 		draw_finish_message();
 		return TIME_ENDED;
 	}
-	
-	draw_time(diff);	
+
+	draw_time(g_time_backup);		
 	return 1;
+}
+
+void timer_pause()
+{
+	draw_paused_state();
+	g_timer_interrupt = 1;
 }
--- timer.h
+++ timer.h
@@ -4,7 +4,9 @@
 #define SCALE_HOURS 1
 
 #define TIME_ENDED 0
+#define INTERR -1
 
 int timer_update_timer();
 void timer_set_time(int time, short time_scale);
 void timer_initialize();
+void timer_pause();
