00571f672d37363aaaa6cea6c8a33f4e31972782
karin <matsukari66@gmail.com>
2026-05-03 08:47:43 -0300

    resume added and code improvement

--- timer.c
+++ timer.c
@@ -12,10 +12,10 @@ WINDOW *g_timer_win;
 
 static void draw_finish_message();
 static WINDOW *build_timer();
-static void draw_time(int time);
+static void draw_time(double time);
 
-int g_timer_interrupt, g_time_upper_limit, g_timer_finished, g_time_backup;
-clock_t g_sys_time_base;
+int g_timer_interrupt, g_time_goal, g_timer_finished;
+clock_t g_time_t0, g_time_pause_adjust, g_pause_t0;
 
 static WINDOW *build_timer()
 {
@@ -35,11 +35,11 @@ static void draw_finish_message()
 	wrefresh(g_timer_win);
 }
 
-static void draw_time(int time)
+static void draw_time(double time)
 {
 	werase(g_timer_win);
 	box(g_timer_win, 0, 0);
-	mvwprintw(g_timer_win, TIMER_LINES/2, (TIMER_COLS - NUM_PADDING)/2, "%0*ld", NUM_PADDING, (long) time);	
+	mvwprintw(g_timer_win, TIMER_LINES/2, (TIMER_COLS - NUM_PADDING)/2, "%0*lf", NUM_PADDING, time);	
 	wrefresh(g_timer_win);
 }
 
@@ -57,10 +57,12 @@ INTERFACE
 
 void timer_initialize()
 {
-	g_timer_finished = 0;
-	g_timer_interrupt = 0;
-	g_time_backup = 0;
-	time(&g_sys_time_base);
+	g_timer_finished = -1;
+	g_timer_interrupt = -1;
+	
+	g_time_pause_adjust = 0;
+	g_pause_t0 = 0;
+	time(&g_time_t0);
 	
 	g_timer_win = build_timer();
 }
@@ -70,33 +72,59 @@ void timer_set_time(int time, short time_scale)
 	switch(time_scale)
 	{
 		case SCALE_MINUTES:
-			g_time_upper_limit = time * 60;
+			g_time_goal = time * 1;
 			break;
 	}
 }
 
-int timer_update_timer()
+int timer_update_time()
 {
-	if (g_timer_finished) return TIME_ENDED;
-	if (g_timer_interrupt) return INTERR;
+	if (g_timer_finished == TIME_ENDED) return TIME_ENDED;
+	if (g_timer_interrupt == INTERR) return INTERR;
+
+	time_t time_t1;
+	time(&time_t1);
+
+	double elapsed_time = difftime(time_t1 - g_time_pause_adjust, g_time_t0);
 	
-	time_t current_sys_time;
-	time(&current_sys_time);
-	g_time_backup = difftime(current_sys_time, g_sys_time_base);
-	
-	if (g_time_backup >= g_time_upper_limit)
+	if (elapsed_time >= g_time_goal)
 	{
-		g_timer_finished = 1;
+		g_timer_finished = TIME_ENDED;
 		draw_finish_message();
 		return TIME_ENDED;
 	}
 
-	draw_time(g_time_backup);		
+	draw_time(elapsed_time);		
+
 	return 1;
 }
 
 void timer_pause()
 {
-	draw_paused_state();
-	g_timer_interrupt = 1;
+	if (g_timer_interrupt == INTERR) return;
+
+	if (g_timer_finished != TIME_ENDED)
+		draw_paused_state();
+
+	g_timer_interrupt = INTERR;
+	time(&g_pause_t0);
 }
+
+void timer_resume()
+{
+	if (g_timer_interrupt == -1) return;
+	if (g_timer_finished == TIME_ENDED) return;
+	
+	g_timer_interrupt = -1;
+	clock_t t1;
+	time(&t1);
+	// how many time since pause
+	g_time_pause_adjust += t1 - g_pause_t0;
+}
+
+void timer_delwin()
+{
+	delwin(g_timer_win);	
+}
+
+
--- timer.h
+++ timer.h
@@ -4,9 +4,11 @@
 #define SCALE_HOURS 1
 
 #define TIME_ENDED 0
-#define INTERR -1
+#define INTERR 1
 
-int timer_update_timer();
+int timer_update_time();
 void timer_set_time(int time, short time_scale);
 void timer_initialize();
 void timer_pause();
+void timer_delwin();
+void timer_resume();
