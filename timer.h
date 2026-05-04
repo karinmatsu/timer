b72062e2d91041613d2a200a8f5c66fc50dfbba1
karin <matsukari66@gmail.com>
2026-05-04 14:22:45 -0300

    pragma once removed

--- timer.h
+++ timer.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef TIMER_H
+#define TIMER_H
 
 #define SCALE_MINUTES 0
 #define SCALE_HOURS 1
@@ -10,18 +11,19 @@
 build the window and initilize it's variables.
 Should be callend before all other timer funcion.
 */
-void timer_initialize();
+void timer_initialize(void);
 /*
 used to specify how long the timer should count.
 'time' will be interpreted based on 'time_scale'.
 should be called before timer_update_window().
 */
-void timer_set_time(int time, short time_scale);
+int timer_set_time(int time, short time_scale);
 
 /*update the timer window and it's current time.
 Uses the current machine time to calculate the time.
 */
-int timer_update_time();
-void timer_pause();
-void timer_delwin();
-void timer_resume();
+int timer_update_time(void);
+
+void timer_pause(void);
+void timer_delwin(void);
+void timer_resume(void);
